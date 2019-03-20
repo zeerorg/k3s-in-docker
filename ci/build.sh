48c79487df192e57267d7261ab4fb3f787283a96
Rishabh Gupta <r.g.gupta@outlook.com>
2019-03-20 13:28:09 +0530

    Removed parallel build

--- ci/build.sh
+++ ci/build.sh
@@ -5,9 +5,12 @@ export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER=x86_64-w64-mingw32-gcc
 
-cargo build --verbose --target x86_64-pc-windows-gnu &
-cargo build --verbode --target x86_64-unknown-linux-gnu &
-cargo build --verbose --target armv7-unknown-linux-gnueabihf &
-cargo build --verbose --target aarch64-unknown-linux-gnu &
-
-wait
+targets=("x86_64-pc-windows-gnu" "x86_64-unknown-linux-gnu" "armv7-unknown-linux-gnueabihf" "aarch64-unknown-linux-gnu")
+for trgt in ${targets[*]}
+do
+  cargo build --target $trgt --verbose
+  if [ "$?" != "0" ]
+  then
+    exit 1
+  fi
+done
