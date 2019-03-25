7dea4c7ec170144f3b69da488b7d713a908785a8
Rishabh Gupta <r.g.gupta@outlook.com>
2019-03-25 21:06:22 +0530

    Adds static compiling to x86_64 linux and armhf targets
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,14 +2,14 @@
 
 linux_build() {
 # Set linker environment variables
-export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=arm-linux-gnueabihf-gcc
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER=x86_64-w64-mingw32-gcc
 
-targets=("x86_64-pc-windows-gnu" "x86_64-unknown-linux-gnu" "armv7-unknown-linux-gnueabihf" "aarch64-unknown-linux-gnu")
+targets=("x86_64-pc-windows-gnu" "x86_64-unknown-linux-musl" "armv7-unknown-linux-musleabihf" "aarch64-unknown-linux-gnu")
 for trgt in ${targets[*]}
 do
-  cargo build --release --target $trgt --verbose
+  cargo build --verbose --target $trgt
   if [ "$?" != "0" ]
   then
     exit 1
@@ -18,8 +18,8 @@ done
 
 mkdir -p target/releases/
 cp target/x86_64-pc-windows-gnu/release/k3d.exe target/releases/k3d.exe
-cp target/x86_64-unknown-linux-gnu/release/k3d target/releases/k3d
-cp target/armv7-unknown-linux-gnueabihf/release/k3d target/releases/k3d-armhf
+cp target/x86_64-unknown-linux-musl/release/k3d target/releases/k3d
+cp target/armv7-unknown-linux-musleabihf/release/k3d target/releases/k3d-armhf
 cp target/aarch64-unknown-linux-gnu/release/k3d target/releases/k3d-arm64
 
 cd target/releases/
