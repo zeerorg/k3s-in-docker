159ecb9a9c81e66e4126ee52e860d8aeb8b8c193
Rishabh Gupta <r.g.gupta@outlook.com>
2019-03-31 15:24:22 +0530

    Added parallel travis builds (#5)
    * Added parallel travis builds
    
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>
    
    * Fixed case end, removed build and pre-build
    
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>
    
    * Fixed release copying
    
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>
    
    * Fixed all release folder errors
    
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>
    
    * Fixed windows build copying by adding OG_FILE
    
    Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,54 +1,42 @@
 #!/bin/sh
 
-linux_build() {
-# Set linker environment variables
-export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=arm-linux-gnueabihf-gcc
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER=x86_64-w64-mingw32-gcc
+rustup target add $TARGET
+export OG_FILE="k3d"
 
-targets=("x86_64-pc-windows-gnu" "x86_64-unknown-linux-musl" "armv7-unknown-linux-musleabihf" "aarch64-unknown-linux-gnu")
-for trgt in ${targets[*]}
-do
-  cargo build --release --verbose --target $trgt
-  if [ "$?" != "0" ]
-  then
-    exit 1
-  fi
-done
+case $TARGET in
+"x86_64-pc-windows-gnu")
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends g++-mingw-w64-x86-64
+  rustup target add x86_64-pc-windows-gnu
+  export FILENAME="k3d.exe"
+  export OG_FILE="k3d.exe"
+  ;;
 
-mkdir -p target/releases/
-cp target/x86_64-pc-windows-gnu/release/k3d.exe target/releases/k3d.exe
-cp target/x86_64-unknown-linux-musl/release/k3d target/releases/k3d
-cp target/armv7-unknown-linux-musleabihf/release/k3d target/releases/k3d-armhf
-cp target/aarch64-unknown-linux-gnu/release/k3d target/releases/k3d-arm64
+"x86_64-unknown-linux-musl")
+  export FILENAME="k3d"
+  ;;
 
-cd target/releases/
-for filename in ./k3d*; do
-  sha256sum $filename > $filename.sha256
-done
-cd ../../
-}
+"armv7-unknown-linux-musleabihf")
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends g++-arm-linux-gnueabihf libc6-dev-armhf-cross
+  export FILENAME="k3d-armhf"
+  ;;
 
-osx_build() {
-cargo build --release --verbose
-if [ "$?" != "0" ]
-then
-  exit 1
-fi
-mkdir -p target/releases/
-cp target/release/k3d target/releases/k3d-darwin
+"aarch64-unknown-linux-gnu")
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends g++-aarch64-linux-gnu libc6-dev-arm64-cross
+  export FILENAME="k3d-arm64"
+  ;;
 
-cd target/releases/
-shasum -a 256 ./k3d-darwin > k3d-darwin.sha256
-
-./k3d-darwin --help
-if [ "$?" != "0" ]
-then
-  exit 1
-fi
-}
-
-case "$(uname -s)" in
-    Linux*)     linux_build;;
-    Darwin*)    osx_build;;
+"x86_64-apple-darwin")
+  export FILENAME="k3d-darwin"
+  ;;
 esac
+
+cargo build --release --verbose --target $TARGET
+
+mkdir -p target/releases/
+mv target/$TARGET/release/$OG_FILE target/releases/$FILENAME
+sha256sum target/releases/$FILENAME > target/releases/$FILENAME.sha256
+
+ls -la target/releases
